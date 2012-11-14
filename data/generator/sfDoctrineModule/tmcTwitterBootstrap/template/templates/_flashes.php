591f956c892208d8d894b3c322d39be745c55ee8
Matías Alejo Roldán Zukerfeld <matiasalejoroldan@gmail.com>
2012-11-14 11:16:05 -0300

    Saco las palabras error, success y notice de los flash messages

--- data/generator/sfDoctrineModule/tmcTwitterBootstrap/template/templates/_flashes.php
+++ data/generator/sfDoctrineModule/tmcTwitterBootstrap/template/templates/_flashes.php
@@ -1,6 +1,5 @@
 [?php if ($sf_user->hasFlash('success')): ?]
     <div class="alert alert-success">
-        <h4 class="alert-heading">[?php echo __('Success', null, 'tmcTwitterBootstrapPlugin') ?]</h4>
         [?php echo __($sf_user->getFlash('success'), array(), '<?php echo $this->getI18nCatalogue() ?>') ?]
     </div>
     [?php $sf_user->setAttribute('success', 'true', 'symfony/user/sfUser/flash/remove') ?]
@@ -8,7 +7,6 @@
 
 [?php if ($sf_user->hasFlash('notice')): ?]
     <div class="alert alert-warning">
-        <h4 class="alert-heading">[?php echo __('Warning', null, 'tmcTwitterBootstrapPlugin') ?]</h4>
         [?php echo __($sf_user->getFlash('notice'), array(), '<?php echo $this->getI18nCatalogue() ?>') ?]
     </div>
     [?php $sf_user->setAttribute('notice', 'true', 'symfony/user/sfUser/flash/remove') ?]
@@ -16,7 +14,6 @@
 
 [?php if ($sf_user->hasFlash('error')): ?]
     <div class="alert alert-error">
-        <h4 class="alert-heading">[?php echo __('Error', null, 'tmcTwitterBootstrapPlugin') ?]</h4>
         [?php echo __($sf_user->getFlash('error'), array(), '<?php echo $this->getI18nCatalogue() ?>') ?]
     </div>
     [?php $sf_user->setAttribute('error', 'true', 'symfony/user/sfUser/flash/remove') ?]
